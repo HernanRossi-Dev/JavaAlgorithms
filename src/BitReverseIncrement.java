d2209d9129e0e6c9b5596793da2fb2758ffd64c7
hernan <hernan_rossi@msn.com>
2018-02-01 18:47:30 -0800

    Implemented a bit reversed counter using BitSet

--- src/BitReverseIncrement.java
+++ src/BitReverseIncrement.java
@@ -1,29 +1,65 @@
 import java.util.BitSet;
 
 public class BitReverseIncrement {
-    private BitSet BitReverseCounter;
-    private BitSet Carry;
+    private BitSet BitReverseCounter, Carry, PrevAns, PrevCarry, Ans;
     private BitSet ReverseOne;
-    private int carry = 0;
-    private int prevAns = 0;
-    private int prevCarry = 0;
     private int numBits;
 
     BitReverseIncrement(int k){
         numBits =k;
         BitReverseCounter = new BitSet(k);
-        Carry = new BitSet(k);
         ReverseOne = new BitSet(k);
         ReverseOne.set(k-1);
-
-        System.out.println("The reverse one bit mask value is:");
+        printBits(ReverseOne);
         printValue(ReverseOne);
     }
 
     public void incrementCounter(){
+        PrevAns = new BitSet();
+        PrevCarry = new BitSet();
+        Ans = (BitSet)BitReverseCounter.clone();
+        Ans.xor(ReverseOne);
+        Carry = (BitSet)BitReverseCounter.clone();
+        Carry.and(ReverseOne);
+        System.out.print("Carry is: ");
+        printBits(Carry);
+        System.out.print("");
+        int setBitIndex =0;
+        while(Carry.cardinality() != 0){
+            setBitIndex = Carry.nextSetBit(0);
 
+            Carry.flip(setBitIndex);
+            if(setBitIndex -1 < 0){
 
+            }else{
+                Carry.flip(setBitIndex-1);
+            }
+            System.out.print("Carry is: ");
+            printBits(Carry);
+            System.out.print("");
+            PrevAns = (BitSet) Ans.clone();
+            PrevCarry = (BitSet) Carry.clone();
+
+            Ans.xor(PrevCarry);
+            Carry.and(PrevAns);
+        }
+        BitReverseCounter = (BitSet)Ans.clone();
+        System.out.println("The incremented counter is: ");
+        printBits(BitReverseCounter);
         printValue(BitReverseCounter);
+    }
+
+    public void printBits(BitSet bitArray){
+        System.out.println("Printing the bits of the array: ");
+        for(int i = numBits -1 ; i >= 0; i--){
+            if(bitArray.get(i)){
+                System.out.print(" [1]");
+            } else {
+                System.out.print(" [0]");
+            }
+
+        }
+        System.out.println("\n");
     }
 
     public void printValue(BitSet bitArray) {
@@ -38,8 +74,9 @@ public class BitReverseIncrement {
 
     public static void main(String[] args) {
         BitReverseIncrement reverseBitCounter = new BitReverseIncrement(4);
-        reverseBitCounter.incrementCounter();
-
+        for(int i =0; i< 3; i++) {
+            System.out.print("\n**** Incrementing\n");
+            reverseBitCounter.incrementCounter();
+        }
     }
-
 }
